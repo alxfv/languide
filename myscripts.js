ffec4d7de46abb7c979e52e85c80424f23ac798d
Aleksandr Fedorov <faostb@gmail.com>
2016-09-06 16:02:40 +0300

    fix body page

--- myscripts.js
+++ myscripts.js
@@ -9,10 +9,29 @@ if (item === null) {
 }
 console.log(activeIds);
 
+//var items = document.getElementById('svg').contentDocument.querySelectorAll('path, ellipse, rect');
+//Array.from(items).forEach((item) => {
+//  var fill = item.style['fill'];
+//  if (typeof fill !== 'undefined' && fill === 'rgb(0, 0, 0)') {
+//    item.remove();
+//  }
+//});
+
 svg.addEventListener('load', () => {
   var doc = svg.contentDocument;
-  var paths = doc.querySelectorAll('path');
-  var ellipses = doc.querySelectorAll('ellipse');
+  var paths = doc.querySelectorAll('path, ellipse, rect');
+
+  Array.from(paths).forEach((item) => {
+    var fill = item.style['fill'];
+    if (typeof fill !== 'undefined' && fill === 'rgb(0, 0, 0)') {
+      //item.style['filter'] = '';
+    }
+
+    var opacity = item.style['opacity'];
+    if (typeof opacity !== 'undefined' && opacity == '0.5') {
+      item.style['opacity'] = '0.1';
+    }
+  });
 
   function setOpacity(path, disable) {
     let id = path.id.match(/[0-9]+/)[0];
@@ -23,13 +42,13 @@ svg.addEventListener('load', () => {
       var item = doc.getElementById(id);
       if (item !== null) {
 
-        if (id.substr(-1) === 'e' && disable === false) {
-          item.style.opacity = '0.5';
-        } else if (id.substr(-1) === 'e' && disable === true) {
-          item.style.opacity = '0.05';
-        } else {
+        //if (id.substr(-1) === 'e' && disable === false) {
+        //  item.style.opacity = '0.5';
+        //} else if (id.substr(-1) === 'e' && disable === true) {
+        //  item.style.opacity = '0.05';
+        //} else {
           item.style.opacity = disable ? opacity : '';
-        }
+        //}
 
         if (disable === true) {
           item.style.top = '700px';
@@ -64,7 +83,6 @@ svg.addEventListener('load', () => {
 
   };
   Array.from(paths).forEach(pathHandler);
-  Array.from(ellipses).forEach(pathHandler);
 
   //document.getElementById('svg').contentDocument.getElementById('29').addEventListener('mouseover', (e) => {
   //  e.stopImmediatePropagation();
@@ -73,14 +91,6 @@ svg.addEventListener('load', () => {
   //});
 
   Array.from(paths).forEach((path) => {
-    let id = path.id;
-    console.log(id, activeIds.includes(id));
-    if (id !== '' && activeIds.includes(id)) {
-      setOpacity(path, true);
-    }
-  });
-
-  Array.from(ellipses).forEach((path) => {
     let id = path.id;
     console.log(id, activeIds.includes(id));
     if (id !== '' && activeIds.includes(id)) {
